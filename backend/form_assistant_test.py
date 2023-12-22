ccfa92c0e041d737e1e4058effc60bcd11239d6d
Frank Yan <frankyan.work@gmail.com>
2023-12-22 15:26:12 +0000

    multiple input

--- backend/form_assistant_test.py
+++ backend/form_assistant_test.py
@@ -3,9 +3,20 @@ from form_assistant import FormAssistant
 
 startup()
 assistant = FormAssistant()
-#output = assistant.ask__question("who are you?")
-#output = assistant.ask__question("Send an email to \"Frank Yan <frankyan.work@gmail.com>\" to inform him of a meeting at the company at nine o'clock tomorrow morning")
-output = assistant.ask__question("Olga want to spend $1000 to lay concrete walkway in south of housing")
-print(output)
-print()
-print(output["output"])
+
+# #output = assistant.ask__question("who are you?")
+# #output = assistant.ask__question("Send an email to \"Frank Yan <frankyan.work@gmail.com>\" to inform him of a meeting at the company at nine o'clock tomorrow morning")
+# output = assistant.ask__question("Olga want to spend $1000 to lay concrete walkway in south of housing")
+# print(output)
+# print()
+# print(output["output"])
+
+inputs = [
+    "who are you?",
+    "Olga want to spend $1000 to lay concrete walkway in south of housing",
+]
+for input in inputs:
+    print()
+    print(input)
+    output = assistant.ask__question(input)
+    print(output["output"])
